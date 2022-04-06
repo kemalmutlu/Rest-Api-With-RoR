162ef21c9dc9a3085fad16da94f3e7bfe9d39b89
Kemal Mutlu <mutlukemal1717@gmail.com>
2022-04-07 02:08:21 +0300

    [add] create session success

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -2,5 +2,11 @@ class AccessTokensController < ApplicationController
   def create
     authenticator = UserAuthenticator.new(params[:code])
     authenticator.perform
+    result = serializer.new(authenticator.access_token)
+    render json: result, status: :created
+  end
+
+  def serializer
+    AccessTokenSerializer
   end
 end
--- app/models/access_token.rb
+++ app/models/access_token.rb
@@ -5,7 +5,7 @@ class AccessToken < ApplicationRecord
   private
   def generate_token
     loop do
-      break if token.present? && !AccessToken.exists?(token: token)
+      break if token.present? && !AccessToken.where.not(id: id).exists?(token: token)
       self.token = SecureRandom.hex(10)
     end
   end
